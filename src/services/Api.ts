92a6430a81b7dde6c96561e835cd3d50f598d5c9
Andi Han <andi.han.2013@gmail.com>
2020-04-04 21:06:25 +1100

    Add fetch Polyfill to avoid errors from browser not supporting fetch

--- src/services/Api.ts
+++ src/services/Api.ts
@@ -1,9 +1,11 @@
 import Owner from '../models/owner'
+import 'whatwg-fetch'
 
 const APIURL = 'http://agl-developer-test.azurewebsites.net/'
 
 async function loadData(objName: string): Promise<Owner[]> {
-    const response = await fetch(`${APIURL}${objName}.json`)
+    // Use fetchPolyfill to avoid errors from the browsers that don't support fetch
+    const response = await window.fetch(`${APIURL}${objName}.json`)
     return response.json()
 }
 
